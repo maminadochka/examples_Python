482858a13391fcf971eaa4cc4efd33bf43682108
maminadochka <83668537+maminadochka@users.noreply.github.com>
2022-09-09 20:49:58 +0300

    Delete hometask3 directory

--- hometask3/task_2.py
+++
@@ -1,7 +0,0 @@
-text = input("Enter text: ")
-
-while "(" in text or ")" in text:
-    open_bkt = text.rfind("(")
-    close_bkt = text.find(")", open_bkt)
-    text = text.replace(text[open_bkt:close_bkt+1], "")
-print(text)
