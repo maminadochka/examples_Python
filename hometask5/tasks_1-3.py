7bdb71077ae93cdbb12d53e9e51d4c7da60c7b07
maminadochka <83668537+maminadochka@users.noreply.github.com>
2022-09-09 20:50:29 +0300

    Delete hometask5 directory

--- hometask5/tasks_1-3.py
+++
@@ -1,6 +0,0 @@
-print(list(map(lambda x: "Even" if x % 2 == 0 else "Odd", [1, 2, 3, 4, 6, 8])))
-
-print(list(map(lambda x: str(x), [1, 2, 3, 4, 6, 8])))
-
-my_tuple = ("kek", "lol", "loop")
-print(list(filter(lambda x: str(''.join(reversed(x))) == x, my_tuple)))
